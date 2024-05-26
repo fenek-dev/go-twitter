0ff9e5bd9582227162f8384f2940ebb3c744fbbc
MaksFenek <makstreonin@gmail.com>
2024-05-26 12:17:54 +0300

    Storage methods

--- src/sso/storage/pg/conn.go
+++ src/sso/storage/pg/conn.go
@@ -2,7 +2,9 @@ package pg
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/fenek-dev/go-twitter/src/common/models"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -23,4 +25,30 @@ func New(ctx context.Context, DBUrl string) *Storage {
 	}
 
 	return &Storage{db: conn}
+}
+
+func (s *Storage) SaveUser(ctx context.Context, username string, passHash []byte) (string, error) {
+	const op = "storage.pg.SaveUser"
+
+	var usrname string
+	err := s.db.QueryRow(ctx, "INSERT INTO users(username, password) VALUES(?, ?) RETURNING username", username, passHash).Scan(&usrname)
+
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return usrname, nil
+}
+
+func (s *Storage) User(ctx context.Context, username string) (models.User, error) {
+	const op = "storage.pg.User"
+
+	var user models.User
+	err := s.db.QueryRow(ctx, "SELECT username, password FROM users WHERE username = ?", username).Scan(&user)
+
+	if err != nil {
+		return user, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
 }
