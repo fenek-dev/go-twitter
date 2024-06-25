9e529ecfb72677cd8d9884b7cdcd2b929c9ddc72
fenek-dev <makstreonin@gmail.com>
2024-06-25 23:06:07 +0300

    Add mapper from token claims to model user

--- src/common/mappers/user.go
+++ src/common/mappers/user.go
@@ -3,25 +3,31 @@ package mappers
 import (
 	proto "github.com/fenek-dev/go-twitter/proto/protogen"
 	"github.com/fenek-dev/go-twitter/src/common/models"
+	"github.com/golang-jwt/jwt/v5"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func UserModelToProtoUser(user *models.User) *proto.User {
 	return &proto.User{
 		Username:    user.Username,
-		PassHash:    user.PassHash,
 		Description: user.Description,
 		CreatedAt:   timestamppb.New(user.CreatedAt),
 		UpdatedAt:   timestamppb.New(user.UpdatedAt),
 	}
 }
 
-func ProtoUserToModel(user *proto.User) *models.User {
-	return &models.User{
+func ProtoUserToModel(user *proto.User) models.User {
+	return models.User{
 		Username:    user.Username,
-		PassHash:    user.PassHash,
 		Description: user.Description,
 		CreatedAt:   user.CreatedAt.AsTime(),
 		UpdatedAt:   user.UpdatedAt.AsTime(),
+	}
+}
+
+func ClaimsToUserModel(claims jwt.MapClaims) *models.User {
+	return &models.User{
+		Username:    claims["username"].(string),
+		Description: claims["description"].(string),
 	}
 }
