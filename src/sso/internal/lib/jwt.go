95918bed1d08c9fc79f1c8763c9384460a6d90a0
fenek-dev <makstreonin@gmail.com>
2024-06-25 23:05:31 +0300

    Add jwt token verify util

--- src/sso/internal/lib/jwt.go
+++ src/sso/internal/lib/jwt.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/fenek-dev/go-twitter/src/common/models"
@@ -8,11 +9,12 @@ import (
 )
 
 func NewToken(user models.User, secret string, duration time.Duration) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	claims := token.Claims.(jwt.MapClaims)
-	claims["username"] = user.Username
-	claims["exp"] = time.Now().Add(duration).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username":    user.Username,
+			"description": user.Description,
+			"exp":         time.Now().Add(duration).Unix(),
+		})
 
 	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
@@ -20,4 +22,19 @@ func NewToken(user models.User, secret string, duration time.Duration) (string, 
 	}
 
 	return tokenString, nil
+}
+
+func GetFromToken(tokenString, secret string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
 }
