1b8207973a368505df8cafbad817a14de5208ff5
fenek-dev <makstreonin@gmail.com>
2024-06-26 17:39:59 +0300

    Register form

---
+++ src/web/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "../widgets/Forms/Register";
+
+export const RegisterPage = () => {
+  return (
+    <main className="container flex min-h-screen justify-center items-center">
+      <RegisterForm />
+    </main>
+  );
+};
