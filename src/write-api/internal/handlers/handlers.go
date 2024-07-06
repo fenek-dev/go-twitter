16cae6ef044b622b1cef1632f534cfabdd185524
fenek-dev <makstreonin@gmail.com>
2024-07-06 11:59:09 +0300

    Write api auth handler trace

--- src/write-api/internal/handlers/handlers.go
+++ src/write-api/internal/handlers/handlers.go
@@ -4,16 +4,21 @@ import (
 	"log/slog"
 
 	"github.com/fenek-dev/go-twitter/src/write-api/internal/services"
+
+	"go.opentelemetry.io/otel/trace"
 )
 
 type Handlers struct {
-	log     *slog.Logger
 	service *services.Services
+
+	log    *slog.Logger
+	tracer trace.Tracer
 }
 
-func New(service *services.Services, log *slog.Logger) *Handlers {
+func New(service *services.Services, log *slog.Logger, tracer trace.Tracer) *Handlers {
 	return &Handlers{
 		log:     log,
 		service: service,
+		tracer:  tracer,
 	}
 }
