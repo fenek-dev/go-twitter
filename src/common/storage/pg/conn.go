7ff26f22bb91998dd0843b4838d8f230c4307e07
fenek-dev <makstreonin@gmail.com>
2024-07-13 16:49:19 +0300

    Add more detailed error message for pg connection

--- src/common/storage/pg/conn.go
+++ src/common/storage/pg/conn.go
@@ -2,6 +2,7 @@ package pg
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -11,11 +12,11 @@ func New(ctx context.Context, DBUrl string) *pgx.Conn {
 	conn, err := pgx.Connect(ctx, DBUrl)
 
 	if err != nil {
-		panic("can not connect to db")
+		panic(fmt.Sprintf("can not connect to db: %s", err.Error()))
 	}
 
 	if err := conn.Ping(ctx); err != nil {
-		panic("db ping didn't work")
+		panic(fmt.Sprintf("db ping didn't work: %s", err.Error()))
 	}
 
 	return conn
