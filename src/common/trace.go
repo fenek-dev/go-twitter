daf56c8d160911fc230789f8eb666ce56a5dd8a5
fenek-dev <makstreonin@gmail.com>
2024-07-13 10:33:24 +0300

    Init telemetry func

--- src/common/trace.go
+++ src/common/trace.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"log"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-	"go.opentelemetry.io/otel/trace"
 )
 
 // Init returns an instance of Jaeger Tracer.
-func Init(ctx context.Context, service string) trace.Tracer {
+func Init(ctx context.Context, service string) *sdktrace.TracerProvider {
 	client := otlptracegrpc.NewClient(
 		otlptracegrpc.WithInsecure(),
 	)
@@ -27,7 +28,12 @@ func Init(ctx context.Context, service string) trace.Tracer {
 		sdktrace.WithResource(newResource(service)),
 	)
 
-	return tp.Tracer(service)
+	otel.SetTracerProvider(tp)
+
+	// Set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	return tp
 }
 
 func newResource(service string) *resource.Resource {
@@ -36,4 +42,25 @@ func newResource(service string) *resource.Resource {
 		semconv.ServiceName(service),
 		semconv.ServiceVersion("0.0.1"),
 	)
+}
+
+func InjectMetadataIntoContext(ctx context.Context, metadata map[string]string) context.Context {
+	propagator := otel.GetTextMapPropagator()
+
+	return propagator.Extract(
+		ctx,
+		propagation.MapCarrier(metadata),
+	)
+}
+
+func ExtractMetadataFromContext(ctx context.Context) map[string]string {
+	propagator := otel.GetTextMapPropagator()
+
+	metadata := map[string]string{}
+	propagator.Inject(
+		ctx,
+		propagation.MapCarrier(metadata),
+	)
+
+	return metadata
 }
